9988ab9aec5fd03bf56249b68c62e761457e2f59
thydd <thyaagooliveir4@gmail.com>
2026-03-24 16:26:27 -0300

    16h à 17h validation.

--- src/DesafioFastBackend.Application/UseCases/Presencas/Create/CreatePresencaValidator.cs
+++ src/DesafioFastBackend.Application/UseCases/Presencas/Create/CreatePresencaValidator.cs
@@ -14,9 +14,16 @@ public class CreatePresencaValidator : AbstractValidator<CreatePresencaInputDto>
             .GreaterThan(0).WithMessage("ColaboradorId deve ser maior que zero.");
 
         RuleFor(x => x.DataHoraCheckIn)
-            .NotEqual(default(DateTime)).WithMessage("DataHoraCheckIn é obrigatória.");
+            .NotEqual(default(DateTime)).WithMessage("DataHoraCheckIn é obrigatória.")
+            .Must(BeWithinWorkshopWindow).WithMessage("DataHoraCheckIn deve estar entre 16:00 e 17:00.");
 
         RuleFor(x => x.Status)
             .IsInEnum().WithMessage("Status inválido.");
+    }
+
+    private static bool BeWithinWorkshopWindow(DateTime checkIn)
+    {
+        var time = checkIn.TimeOfDay;
+        return time >= new TimeSpan(16, 0, 0) && time <= new TimeSpan(17, 0, 0);
     }
 }
