54b47b6deb65027d73d343970b8af70e56a01c6e
thydd <thyaagooliveir4@gmail.com>
2026-03-24 18:20:17 -0300

    migration

---
+++ src/DesafioFastBackend.Infrastructure/Migrations/20260324211810_AjustesFinais.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesafioFastBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustesFinais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
